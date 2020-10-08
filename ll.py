3478f5e03e2b916db53f3e095e06986766b06354
himanshusoni2708 <himanshusoni2708@gmail.com>
2020-10-08 10:05:39 +0530

    append,pop and print

---
+++ ll.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self,a):
+        self.value=a
+        self.next=None
+
+class LList:
+    def __init__(self):
+        self.head=None
+
+    def show(self):
+        first=self.head
+        while True:
+            if first is not None:
+                print(first.value)
+                first=first.next
+            else:
+                break
+
+    def add(self,new):
+        if self.head is None:
+            self.head=new
+
+        else:
+            first=self.head
+            while first.next is not None:
+                first=first.next
+            first.next=new
+
+    def addtop(self,mew):
+        first=self.head
+        self.head=mew
+        mew.next=first
+
+    def pop(self):
+        first=self.head
+        while first.next is not None:
+            prev=first
+            first=first.next
+        prev.next=None
+
+    def pop 
+
+ll=LList()
+n1=Node(19)
+ll.add(n1)
+n2=Node(22)
+ll.add(n2)
+n3=Node(23)
+ll.add(n3)
+n3=Node(25)
+ll.add(n3)
+ll.poptop()
+
+ll.show()
